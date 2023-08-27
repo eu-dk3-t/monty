6ebc15168619c5a9523f06ed11560a203677e8c3
eu-dk3-t <ze_3fanuel@proton.me>
2023-08-27 20:34:56 +0000

    [Fix]: Removed faulty prototypes from header. Rectified errors by removing poor implementations of functions

--- monty.h
+++ monty.h
@@ -50,10 +50,6 @@ extern int misc[];
 void (*command(char *tok))(stack_t **stack, unsigned int line_number);
 void get_line(char *buffer, unsigned int line_number, stack_t **stack);
 void free_stack(stack_t *stack);
-/**
-char* strtok_r(char* str, const char* delim, char** saveptr);
-ssize_t getline(char **lineptr, size_t *n, FILE *stream);
-*/
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
